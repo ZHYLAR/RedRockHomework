1625800ff22503d6f596b48382f9d5733a1090f0
ZHYLAR <110972048+ZHYLAR@users.noreply.github.com>
2022-11-05 15:33:13 +0800

    Add files via upload

---
+++ 第四次作业/LV0.js
@@ -0,0 +1,7 @@
+// 你的代码
+const [name,[h1,h2,h3],HOME,year] = ['MING',['play','run','sing'],'HENAN',2022]
+
+console.log(name)  // MING
+console.log(h1,h2,h3)  // play run sing
+console.log(HOME)  // HENAN，这里的HOME就是大写哈
+console.log(year)  // 2022
